940ee6672c5659fb2790c684ecd4d1199e4e6680
Naoto Horiguchi <naoto.horiguchi@gmail.com>
2014-02-03 02:50:22 +0900

    first commit

---
+++ Assets/BallContoller.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var speed = 5;
+
+function Start () {
+	rigidbody.AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+}
+
+function Update () {
+
+}
