15377ab448f489b5e4e41fe3a108df773eb1cac7
Naoto Horiguchi <naoto.horiguchi@gmail.com>
2014-02-15 22:01:24 +0900

    practice03

--- Assets/BallContoller.js
+++ Assets/BallContoller.js
@@ -7,12 +7,24 @@ function Start () {
 }
 
 function Update () {
-
+	var blocks = GameObject.Find("Blocks");
+	
+	// Check count of remaining blocks.
+	if (blocks.transform.childCount == 0) {
+    		Debug.Log("Clear!!");
+    		Destroy(this.gameObject);	
+    }
 }
 
 function OnCollisionEnter(col:Collision) {
-    var blocks = gameObject.Find("Blocks");
+    var blocks = GameObject.Find("Blocks");
     if (col.transform.IsChildOf(blocks.transform)) {
     	Destroy(col.gameObject);
+    }
+    
+    var bottomBar = GameObject.Find("BottomBar");
+    if (col.gameObject == bottomBar) {
+    	Debug.Log("Game Over");
+    	Destroy(this.gameObject);
     }
 }
